a0d8751e91144ec60b6d8710e95c563f122411de
Utkarsh Goel <ugoel911@gmail.com>
2021-12-02 01:20:07 +0530

    cache connected to cloud

--- db/redis.js
+++ db/redis.js
@@ -1,8 +1,11 @@
 const redis = require("redis");
 
 const REDIS_PORT = process.env.PORT || 6379;
-console.log(REDIS_PORT);
 
-const client = redis.createClient(REDIS_PORT);
+const client = redis.createClient({
+    url: process.env.REDIS_HOST,
+    port: REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+});
 
 module.exports = client;
